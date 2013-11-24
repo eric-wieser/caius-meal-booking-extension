26a0a98e6c27a2ccfa5b0f710c51f7f72d3bf636
Eric Wieser <wieser.eric@gmail.com>
2013-11-24 22:29:51 +0000

    Adds loading of profile information

--- background.js
+++ background.js
@@ -1,17 +1,29 @@
+var profileLoad = $.get('https://www.mealbookings.cai.cam.ac.uk/profile.php').then(function(data) {
+	return {
+		vegetarian: $(data).find('input[name=vegetarian]').val() == "1",
+		requirements: $(data).find('input[name=requirements]').val(),
+	};
+})
+
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
 	if(request.action == 'getTemplates') {
 		var templates = document.querySelectorAll('#templates > div');
-		var toSend = {};
+		var templatePayload = {};
 
 		for (var i = 0; i < templates.length; i++) {
 			var name = templates[i].getAttribute('data-name');
 			var theme = templates[i].getAttribute('data-theme') || 'default';
-			if(toSend[name] == null)
-				toSend[name] = {};
+			if(templatePayload[name] == null)
+				templatePayload[name] = {};
 
-			toSend[name][theme] = templates[i].innerHTML;
+			templatePayload[name][theme] = templates[i].innerHTML;
 		}
 
-		sendResponse(toSend);
+		sendResponse(templatePayload);
+	}
+	else if(request.action == 'getPreferences') {
+		profileLoad.then(function(x) {
+			sendResponse(x);
+		});
 	}
 });
