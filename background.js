eric-wieser/caius-meal-booking-extension

commit 5234316a08402bdfbf7e07a3db068474c40f3392

Dynamic loading of hall types. Too much data.
 - Need to only load things as they're needed
 - Work out what's cacheable and where to cache it

background.js
@@ -3,7 +3,27 @@ var profileLoad = $.get('https://www.mealbookings.cai.cam.ac.uk/profile.php').th
 		vegetarian: $(data).find('input[name=vegetarian]').prop('checked'),
 		requirements: $(data).find('input[name=requirements]').val(),
 	};
-})
+});
+
+
+var pageLoaders = Number.range(0, 350).every().map(function(i) {
+	return $.get('https://www.mealbookings.cai.cam.ac.uk/index.php', {event: i}).then(function(d) {
+		var header = $(d).find('h1');
+		var title = header.text();
+		var description = header.nextAll('p').first().text();
+		if(/^Current bookings for/.test(title))
+			return undefined;
+		else if(/^Error:/.test(title))
+			return null;
+		else
+			return {id: i, name: title, description: description}
+	})
+});
+
+var hallNameLoad = $.whenAll(pageLoaders).then(function(titles) {
+	// remove non-existant halls
+	return titles.filter(function(x) { return x; });
+});
 
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
 	if(request.action == 'getTemplates') {
@@ -23,6 +43,11 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
 	}
 	else if(request.action == 'getPreferences') {
 		profileLoad.then(function(x) {
+			sendResponse(x);
+		});
+	}
+	else if(request.action == 'loadHallTypes') {
+		hallNameLoad.then(function(x) {
 			sendResponse(x);
 		});
 	}

HallSummary.js
@@ -180,7 +180,7 @@ HallSummary.loadAllOfType = function(type) {
 
 		var parseFullness = function(summary, str) {
 
-			var fullnessParts = /\((\d+)\/(\d+)\)/.exec(str)
+			var fullnessParts = /\((-?\d+)\/(\d+)\)/.exec(str)
 			try {
 				summary.capacity = parseInt(fullnessParts[2]);
 				summary.available = parseInt(fullnessParts[1]);
@@ -225,10 +225,15 @@ HallSummary.loadAllOfType = function(type) {
 };
 
 HallSummary.loadAll = function() {
-	var tasks = HallType.all.map(function(t) {
-		return HallSummary.loadAllOfType(t);
-	});
-	return $.whenAll(tasks).then(function(results) {
-		return [].concat.apply([], results);
-	}).promise();
+	return HallType.loadAll()
+		.then(function(types) {
+			var tasks = types.map(function(t) {
+				return HallSummary.loadAllOfType(t);
+			});
+			return $.whenAll(tasks);
+		})
+		.then(function(results) {
+			return [].concat.apply([], results);
+		})
+		.promise();
 };

HallType.js
@@ -3,26 +3,12 @@ var HallType = function(name, id) {
 	this.id = id;
 }
 
-HallType.all = [
-	new HallType('first', 256),
-	new HallType('formal', 257),
-	new HallType('sunday formal', 258),
-	// 259 - 261: unused
-	new HallType('cafeteria', 262),
-	new HallType('pre-term', 263),
-	// 264 - 273: unused
-	new HallType('corporate communion', 274),
-	// 275: Error!
-	new HallType('first (early)', 276),
-	// 277 - 278: unused
-	new HallType('christmas first', 279),
-	new HallType('christmas formal', 280),
-	new HallType('superhall', 282),
-	// 283 - 289: unused
-	new HallType('first', 290),
-	new HallType('formal', 291),
-	new HallType('sunday formal', 292),
-	// 292 - 295: unused
-	new HallType('cafeteria', 296),
-	new HallType('pre-term', 297)
-];
+HallType.loadAll = function() {
+	return $.defer(Object.method(chrome.extension, 'sendRequest'), {action: 'loadHallTypes'}).then(function(data) {
+		data.forEach(function(d) {
+			d.__proto__ = Object.create(HallType.prototype);
+		});
+		console.log("Step 1", data);
+		return data;
+	})
+};

background.html
@@ -1,6 +1,5 @@
 <html>
 	<head>
-		<script type="text/javascript" src="jquery-1.10.2.js"></script>
 		<div id="templates">
 			<div data-name="toolbar">
 				<div class='tools'>
@@ -46,6 +45,9 @@
 				</div>
 			</div>
 		</div>
+		<script type="text/javascript" src="jquery-1.10.2.js"></script>
+		<script type="text/javascript" src="sugar.js"></script>
+		<script type="text/javascript" src="utils.js"></script>
 		<script type="text/javascript" src="background.js"></script>
 	</head>
 </html>

main.js
@@ -57,7 +57,10 @@ $.when(
 		toolbar.appendTo('body');
 		var attendeeWrapper = $('<div>').addClass('attendee-wrapper').appendTo('body');
 
-		HallSummary.loadAll().done(function(all) { all
+		HallSummary.loadAll().done(function(all) {
+			console.log("Eek", all);
+
+			all
 			.sortBy('date')
 			.eachGroup('date', function(date, halls) {
 				// sort formals and firsts

utils.js
@@ -35,11 +35,11 @@ Array.extend({
 });
 
 // jQuery configuration
-$.ajaxPrefilter('html', function(options, originalOptions) {
+$.ajaxPrefilter(function(options, originalOptions) {
 	if(!originalOptions.dataFilter) {
 		// encode root-level tags in a way that they can be interpreted
 		options.dataFilter = function(htmlString) {
-			return htmlString.replace(/<(\/?)(html|head|body)/g, '<$1ajax:$2')
+			return htmlString.replace(/<(\/?)(html|head|body|img)/g, '<$1ajax:$2')
 		};
 	}
 });

commit 899fe1c83771e70c2c66882a4f211d3fedcc12ea

Only shows recent halls
Temporarily undoes the problems caused by 5234316a08402bdfbf7e07a3db068474c40f3392

background.js
@@ -13,7 +13,7 @@ var profileLoad = loggedIn
 
 var hallNameLoad = loggedIn
 	.then(function() {
-		var pageLoaders = Number.range(0, 350).every().map(function(i) {
+		var pageLoaders = Number.range(256, 300).every().map(function(i) {
 			return $.get('https://www.mealbookings.cai.cam.ac.uk/index.php', {event: i}).then(function(d) {
 				var header = $(d).find('h1');
 				var title = header.text();