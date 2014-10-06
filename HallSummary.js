4d556867b2e90511fa018c69ba3f3adc8c1e226c
Eric Wieser <wieser.eric@gmail.com>
2014-10-06 17:38:16 +0100

    Scrape crsids alongside names

--- HallSummary.js
+++ HallSummary.js
@@ -99,14 +99,15 @@ HallSummary.prototype.loadAttendees = function() {
 			attendees = attendees.find('tr').map(function() {
 				var cells = $(this).find('td');
 				var name = cells.eq(0).text();
+				var crsid = cells.eq(0).attr('title');
 				var namedGuestStr = /\((\d+) guests?;(.*)\)/.exec(cells.eq(1).text());
 				var guestStr = /\((\d+) guests?\)/.exec(cells.eq(1).text());
 				if(namedGuestStr)
-					return {name: name, guestCount: namedGuestStr[1], guests: namedGuestStr[2]};
+					return {name: name, crsid: crsid, guestCount: namedGuestStr[1], guests: namedGuestStr[2]};
 				else if(guestStr)
-					return {name: name, guestCount: guestStr[1]};
+					return {name: name, crsid: crsid, guestCount: guestStr[1]};
 				else
-					return {name: name};
+					return {name: name, crsid: crsid};
 			}).get();
 			self.attendees = attendees;
 		}
